a77b2b21a6834fe98ed276d29439d94db674d1f9
LAB-USR-LN6377-A0607 <LAB-USR-LN6377-A0607@UTP362631.grupoutp.net>
2023-09-25 11:15:41 -0500

    Jaramillo

---
+++ commitprueba.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB-USR-LN6377-A0607
+ */
+public class commitprueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }jejejejeje
+    
+}
